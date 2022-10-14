f2910ea42b2b41d113a45fa112f072d2fe44a7b6
obs1230 <obs1230@gmail.com>
2022-10-14 03:31:37 -0500

    various changes - attempting heroku server fix

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
  
 app.get('/', (req, res) => {
 
