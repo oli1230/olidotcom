8cfc304047f9108f73ed71016ca198a748854546
obs1230 <obs1230@gmail.com>
2022-10-14 03:02:59 -0500

    various changes - attempting heroku server fix

--- server.js
+++ server.js
@@ -2,10 +2,54 @@ const express = require('express');
 const app = express();
  
 app.get('/', (req, res) => {
-  res
-    .status(200)
-    .send('Hello server is running')
-    .end();
+
+    if (req.method == 'GET') {
+        var fileUrl;
+        if (req.url == '/') fileUrl = '/index.html';
+        else fileUrl = req.url;
+
+        var filePath = path.resolve('./public' + fileUrl);
+        const fileExt = path.extname(filePath);
+        if (fileExt == '.html') {
+            fs.exists(filePath, (exists) => {
+                if (!exists) {
+                    filePath = path.resolve('./public/404.html');
+                    res.statusCode = 404;
+                    res.setHeader('Content-Type', 'text/html');
+                    fs.createReadStream(filePath).pipe(res);
+                    return;
+                }
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html');
+                fs.createReadStream(filePath).pipe(res);
+            });
+        }
+        else if (fileExt == '.css') {
+            res.statusCode = 200;
+            // res.setHeader('Content-Type', 'text/css');
+            fs.createReadStream(filePath).pipe(res);
+        }
+        else if (fileExt == '.png') {
+            res.statusCode = 200;
+            fs.createReadStream(filePath).pipe(res);
+        }
+        else if (fileExt == '.js') {
+            res.statusCode = 200;
+            fs.createReadStream(filePath).pipe(res);
+        }
+        else {
+            filePath = path.resolve('./public/404.html');
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/html');
+            fs.createReadStream(filePath).pipe(res);
+        }
+    }
+    else {
+        filePath = path.resolve('./public/404.html');
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html');
+        fs.createReadStream(filePath).pipe(res);
+    }
 });
  
 // Start the server
